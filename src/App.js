f45a73dfcdd9555b629913d3e7548fe77fe37183
Kamil <prospero.apps@gmail.com>
2022-09-27 12:41:18 +0200

    Use HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './style.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'; 
 import Home from './pages/Home';
 import Game from './pages/Game';
 import Leaderboard from './pages/Leaderboard';
